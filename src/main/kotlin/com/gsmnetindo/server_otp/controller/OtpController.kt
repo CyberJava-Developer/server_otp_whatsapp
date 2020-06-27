b09da34f1b5ae243a9d1debf3a55f12ea7c802e2
develop-faisal <nurwibowofaisal@gmail.com>
2020-06-28 00:32:05 +0700

    set

--- src/main/kotlin/com/gsmnetindo/server_otp/controller/OtpController.kt
+++ src/main/kotlin/com/gsmnetindo/server_otp/controller/OtpController.kt
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.net.http.HttpRequest
-
 
 @RestController
 @RequestMapping("/api/otp")
